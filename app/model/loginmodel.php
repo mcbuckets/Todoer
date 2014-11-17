887b615567b5fc5807cc28d75e3a2840f7aaaf32
Frano Barać <mcbuckets@users.noreply.github.com>
2014-11-17 18:57:47 +0100

    reg complete

--- app/model/loginmodel.php
+++ app/model/loginmodel.php
@@ -28,50 +28,49 @@ class LoginModel
 			AND !empty($_POST['user_password'])
 			AND !empty($_POST['user_email'])
 			AND filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)){
+			$user_name = strip_tags($_POST['user_name']);
+	        $user_email = strip_tags($_POST['user_email']);
+	        $user_password_hash = password_hash($_POST['user_password'], PASSWORD_DEFAULT);
 
-				$user_name = strip_tags($_POST['user_name']);
-		        $user_email = strip_tags($_POST['user_email']);
-		        $user_password_hash = password_hash($_POST['user_password'], PASSWORD_DEFAULT);
+	        $query = $this->db->prepare("SELECT * FROM user WHERE user_name = :user_name");
+	        $query->execute(array(':user_name' => $user_name));
+	        $count = $query->rowCount();
 
-		        $query = $this->db->prepare("SELECT * FROM user WHERE user_name = :user_name");
-		        $query->execute(array(':user_name' => $user_name));
-		        $count = $query->rowCount();
+	        if($count == 1){
+	        	$_SESSION["feedback_negative"][] = "Username already taken!";
+	        	return false;
+	        }
 
-		        if($count == 1){
-		        	$_SESSION["feedback_negative"][] = "Username already taken!";
-		        	return false;
-		        }
+	        $query = $this->db->prepare("SELECT user_id FROM user WHERE user_email = :user_email");
+	        $query->execute(array(':user_email' => $user_email));
+	        $count = $query->rowCount();
 
-		        $query = $this->db->prepare("SELECT user_id FROM user WHERE user_email = :user_email");
-		        $query->execute(array(':user_email' => $user_email));
-		        $count = $query->rowCount();
+	        if($count == 1){
+	        	$_SESSION["feedback_negative"][] = "E-mail is already taken";
+	        	return false;
+	        }
 
-		        if($count == 1){
-		        	$_SESSION["feedback_negative"][] = "E-mail is already taken";
-		        	return false;
-		        }
+	        $user_creation_timestamp = time();
 
-		        $user_creation_timestamp = time();
+	        $sql = "INSERT INTO user (user_name, user_password_hash, user_email, user_creation_timestamp)
+	                    VALUES (:user_name, :user_password_hash, :user_email, :user_creation_timestamp)";
+	        $query = $this->db->prepare($sql);
+	        $query->execute(array(':user_name' => $user_name,
+	                              ':user_password_hash' => $user_password_hash,
+	                              ':user_email' => $user_email,
+	                              ':user_creation_timestamp' => $user_creation_timestamp));
 
-		        $sql = "INSERT INTO user (user_name, user_password_hash, user_email, user_creation_timestamp)
-		                    VALUES (:user_name, :user_password_hash, :user_email, :user_creation_timestamp)";
-		        $query = $this->db->prepare($sql);
-		        $query->execute(array(':user_name' => $user_name,
-		                              ':user_password_hash' => $user_password_hash,
-		                              ':user_email' => $user_email,
-		                              ':user_creation_timestamp' => $user_creation_timestamp));
+	        $count = $query->rowCount();
+	        if($count != 1){
+	        	$_SESSION["feedback_negative"][] = "Registration failed!";
+	        	return false;
+	        }
 
-		        $count = $query->rowCount();
-		        if($count != 1){
-		        	$_SESSION["feedback_negative"][] = "Registration failed!";
-		        	return false;
-		        }
-
-		        $result_user_row = $query->fetch();
-		        $user_id = $result_user_row->user_id;
+	        $result_user_row = $query->fetch();
+	        $user_id = $result_user_row->user_id;
 
 	    	}else{
-	    $_SESSION['feedback_negative'][] = "Unknown error!";
+	    	$_SESSION['feedback_negative'][] = "Unknown error!";
 		}
 
 		return false;
