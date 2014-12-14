d74a889e07aedb45b0a38c7dffa54b083e6dafdb
Frano Barać <mcbuckets@users.noreply.github.com>
2014-12-14 18:48:52 +0100

    edited db  data

--- app/db_install/create-user-table.sql
+++ app/db_install/create-user-table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `todoer`.`user`(
- `user_id` int(11) NOT NULL AUTO_INCREMENT ,
+ `user_id` int(11) NOT NULL AUTO_INCREMENT,
  `user_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
  `user_lastname` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
- `user_password_hash` varchar255) COLLATE utf8_unicode_ci DEFAULT NULL,
+ `user_password_hash` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
  `user_email` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
  `user_active` tinyint(1) NOT NULL DEFAULT '0',
  `user_creation_timestamp` timestamp DEFAULT CURRENT_TIMESTAMP,
