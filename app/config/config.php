f3b464fab9ed7272b3de0ab7eea9e013a245b8b9
Frano Barać <mcbuckets@users.noreply.github.com>
2014-12-14 18:48:39 +0100

    edited config data

--- app/config/config.php
+++ app/config/config.php
@@ -3,11 +3,11 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-define('URL', 'http://example.com/');
+define('URL', 'localhost/');
 
 define('DB_TYPE', 'mysql');
 define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'todo');
+define('DB_NAME', 'todoer');
 define('DB_USER', 'root');
 define('DB_PASS', 'toor');
 
