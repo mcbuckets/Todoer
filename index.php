f41603abe0343546efd5e3e32c1fa42c8ef39807
Frano Barać <mcbuckets@users.noreply.github.com>
2014-12-14 18:07:19 +0100

    autoloder functionality

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
-	require 'app/config/config.php';
-	require 'app/config/autoload.php';
+require 'app/config/config.php';
+if (file_exists('vendor/autoload.php')) {
+    require 'vendor/autoload.php';
+}
 
-	$app = new Application();
+require 'app/config/autoload.php';
+
+$app = new Application();
