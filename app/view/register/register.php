4d03b4aad6f0b677919631c5875514160cb0f678
Frano Barać <mcbuckets@users.noreply.github.com>
2014-12-18 12:47:21 +0100

    Update register.php

--- app/view/register/register.php
+++ app/view/register/register.php
@@ -9,8 +9,8 @@
 	<a href="<?php echo URL;?>" id="gotoLogin">Go to login page</a>
 	<section id= "main">
 		<form action="<?php echo URL;?>register/register_submit" method="POST" id="form">
-			<p class="field"><input type="text" name="user_name" id="login_input_name" pattern="[a-zA-Z0]" placeholder="Please enter your name (2 < chars < 30, letters only)"required></p>
-			<p class="field"><input type="text" name="user_lastname" id="login_input_lastname" placeholder="Please enter your lastname (2 < chars < 60, letters only):" pattern="[a-zA-Z0]" required></p>
+			<p class="field"><input type="text" name="user_name" id="login_input_name" placeholder="Please enter your name (2 < chars < 30, letters only)"required></p>
+			<p class="field"><input type="text" name="user_lastname" id="login_input_lastname" placeholder="Please enter your lastname (2 < chars < 60, letters only):" required></p>
 		    <p class="field"><input type="email" name="user_email" id="login_input_email" placeholder="Please enter your e-mail:" required /></p>
 			<p class="field"><input id="login_input_password_new" type="password" name="user_password" pattern=".{6,}" placeholder="Please enter your password (min. 6 chars):" required autocomplete="off" /></p>
 			<input type="Submit" id="bttn" value="Submit" class="myButton" />
